3d9597d98099b08b457fe8d3e8b8fcea373fe48c
DarrellBailey <dbaile10@gmail.com>
2017-04-28 15:01:46 -0400

    adding map marker operations

--- app/scripts/controllers/map/controller.map.js
+++ app/scripts/controllers/map/controller.map.js
@@ -1,11 +1,47 @@
 (function() {
     'use strict';
     angular.module("neteoc").controller('mapCtrl', mapCtrl);
-    mapCtrl.$inject = ['$scope'];
+    mapCtrl.$inject = ['$scope', 'leafletData'];
 
-    function mapCtrl($scope) {
+    function mapCtrl($scope, leaflet) {
 
-        $scope.init = function() {}
+        $scope.mapMarkers = [{
+            lat: 32.837,
+            lng: -83.632,
+            message: "<a href='#!/'>Hello</a>",
+            draggable: false
+        }];
+
+        $scope.mapCenter = {
+            lat: 32.837,
+            lng: -83.632,
+            zoom: 10
+        };
+
+        $scope.init = function() {};
+
+        $scope.setMapCenter = function(lat, lng, zoom) {
+            $scope.mapCenter = {
+                lat: lat,
+                lng: lng,
+                zoom: zoom
+            };
+        };
+
+        $scope.addMapMarker = function(lat, lng, message, draggable) {
+            $scope.mapMarkers.push({
+                lat: lat,
+                lng: lng,
+                message: message,
+                draggable: draggable
+            });
+        }
+
+        $scope.mapClick = function(event, args) {
+            $scope.addMapMarker(args.leafletEvent.latlng.lat, args.leafletEvent.latlng.lng, "<a href='#!/'>Hello</a>", false);
+        };
+
+        $scope.$on('leafletDirectiveMap.click', $scope.mapClick);
 
         $scope.init();
     }
