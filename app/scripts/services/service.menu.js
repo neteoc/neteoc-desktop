bda107fa7ea17b08f92e0bcd462e97c416eecfa8
Darrell Bailey <dbailey@buildtechnologygroup.com>
2017-05-02 12:31:29 -0400

    simple formatting

--- app/scripts/services/service.menu.js
+++ app/scripts/services/service.menu.js
@@ -4,29 +4,30 @@
     menu.$inject = ['$rootScope', '$location', 'Menu', 'currentWindow', 'serial'];
 
     function menu($rootScope, $location, Menu, currentWindow, serial) {
-        this.init = function(){
+        this.init = function() {
 
             var menu = Menu.buildFromTemplate([{
-            label: 'Devices',
-            submenu: [{
-                label: 'GPS',
-                submenu: [{
-                    label: "Fake Device 1",
-                    click: function(){
-                        alert('Now Connected To Fake Device 1');
-                    }
-                },{
-                    label: "Fake Device 2",
-                    click: function(){
-                        alert('Now Connected To Fake Device 2');
-                    }
-                }]
-            }]
-        },
-        {
-            label: 'Dev Tools',
-            click: function(){ currentWindow.webContents.openDevTools(); }
-        }]);
+                    label: 'Devices',
+                    submenu: [{
+                        label: 'GPS',
+                        submenu: [{
+                            label: "Fake Device 1",
+                            click: function() {
+                                alert('Now Connected To Fake Device 1');
+                            }
+                        }, {
+                            label: "Fake Device 2",
+                            click: function() {
+                                alert('Now Connected To Fake Device 2');
+                            }
+                        }]
+                    }]
+                },
+                {
+                    label: 'Dev Tools',
+                    click: function() { currentWindow.webContents.openDevTools(); }
+                }
+            ]);
             Menu.setApplicationMenu(menu);
         }
     };
