cb7e48ad35ae1d41ab125ca158adfc9311cf7de9
DarrellBailey <dbaile10@gmail.com>
2017-06-03 21:22:33 -0400

    read from serial ports in serial service

---
+++ app/scripts/services/service.serialPorts.js
@@ -0,0 +1,27 @@
+(function() {
+    'use strict';
+    angular.module('neteoc').service('serialPorts', serialPorts);
+    serialPorts.$inject = ['$rootScope', '$location', 'serialport'];
+
+    function serialPorts($rootScope, $location, serialport) {
+        var self = this;
+
+        self.ports = null;
+
+        self.init = function() {
+            self.list(function(err, ports) { self.ports = ports; })
+        };
+
+        self.list = function(callback) {
+            serialport.list(callback);
+        };
+
+        self.open =function(port, baudRate, callback){
+            var p = new SerialPort(port, baudRate);
+            p.on('data', callback);
+            return p;
+        };
+
+        self.init();
+    };
+})();
