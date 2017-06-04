d3c6a965f3fad8fb947fd927e46af7e351b56eca
DarrellBailey <dbaile10@gmail.com>
2017-06-03 22:27:08 -0400

    serial port read in test

--- app/scripts/controllers/system/controller.serialPorts.js
+++ app/scripts/controllers/system/controller.serialPorts.js
@@ -1,14 +1,24 @@
 (function() {
     'use strict';
     angular.module("neteoc").controller('serialPortsCtrl', serialPortsCtrl);
-    serialPortsCtrl.$inject = ['$scope', '$location', 'serialService'];
+    serialPortsCtrl.$inject = ['$scope', '$location', 'serialPorts'];
 
-    function serialPortsCtrl($scope, $location, serialService) {
+    function serialPortsCtrl($scope, $location, serialPorts) {
 
         $scope.ports = null;
+        $scope.selectedPort = "COM1";
+        $scope.baudRate = "4800";
+        $scope.reads = [];
+
 
         $scope.init = function() {
-            $scope.ports = serialService.ports;
+            $scope.ports = serialPorts.ports;
+        };
+
+        $scope.openPort = function(){
+            serialPorts.open($scope.selectedPort, $scope.baudRate, function(data){
+                reads.push(data);
+            });
         };
 
         $scope.init();
