a1c22551b69cebdf7d3df66f16346464cfa0506f
DarrellBailey <dbaile10@gmail.com>
2017-06-03 22:52:41 -0400

    changed serial menu for other platforms

--- app/scripts/services/service.menu.js
+++ app/scripts/services/service.menu.js
@@ -1,39 +1,33 @@
-(function() {
+(function () {
     'use strict';
     angular.module('neteoc').service('menu', menu);
     menu.$inject = ['$rootScope', '$location', 'Menu', 'currentWindow'];
 
     function menu($rootScope, $location, Menu, currentWindow) {
-        this.init = function() {
+        this.init = function () {
 
             var menu = Menu.buildFromTemplate([{
-                    label: 'Devices',
+                label: 'Devices',
+                submenu: [{
+                    label: 'GPS',
                     submenu: [{
-                        label: 'GPS',
-                        submenu: [{
-                            label: "Fake Device 1",
-                            click: function() {
-                                alert('Now Connected To Fake Device 1');
-                            }
-                        }, {
-                            label: "Fake Device 2",
-                            click: function() {
-                                alert('Now Connected To Fake Device 2');
-                            }
-                        }]
+                        label: "Serial Ports",
+                        click: function () {
+                            $location.path("/serialports");
+                            $rootScope.$apply();
+                        }
+                    }, {
+                        label: "Fake Device 2",
+                        click: function () {
+                            alert('Now Connected To Fake Device 2');
+                        }
                     }]
-                },
-                {
-                    label: 'Dev Tools',
-                    click: function() { currentWindow.webContents.openDevTools(); }
-                },
-                {
-                    label: 'Serial Ports',
-                    click: function() {
-                        $location.path("/serialports");
-                        $rootScope.$apply();
-                    }
-                }
+                }]
+            },
+            {
+                label: 'Dev Tools',
+                click: function () { currentWindow.webContents.openDevTools(); }
+            }
             ]);
             Menu.setApplicationMenu(menu);
         }
