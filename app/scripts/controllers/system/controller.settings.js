b79028951acb175239f9f2277afa2022246b85b5
EricWehrly <eric@disruptiontheory.com>
2017-06-07 20:11:47 -0400

    settings form prettier

--- app/scripts/controllers/system/controller.settings.js
+++ app/scripts/controllers/system/controller.settings.js
@@ -14,6 +14,9 @@
         $scope.addingSerialDevice = false;
         $scope.serialConfigurationWorks = true;
 
+        $scope.maxReads = 32;
+        $scope.gpsReads = 0;
+
         $scope.init = function() {
             serialPorts.list(function(err, ports) { 
 
@@ -22,6 +25,27 @@
                     if(!$scope.ports) $scope.ports = [];
                 }, 1);
             });
+
+            gpsService.onRead(function(data) {
+
+                // TODO: we may want to give this a few tries ...
+                if(data.lat) {
+                    $scope.serialConfigurationWorks = true;
+
+                    // TODO: unsubscribe self instead of clear all
+                    gpsService.clearEvents();
+                } else {
+                    console.log(data);
+
+                    if($scope.gpsReads == $scope.maxReads) {
+                        gpsService.clearEvents();
+                    } else {
+                        $scope.gpsReads++;
+                    }
+                }
+
+                // TODO: close port ...
+            })
 
             // TODO: this should be global, not in this controller ...
             var savedConfig = JSON.parse(localStorage.getItem("gpsSerialConfiguration")) || null;
@@ -36,37 +60,8 @@
 
         $scope.openPort = function(){
 
-            // TODO: Still doesn't get angular to friggin digest right. Guh, Tina!
-            $timeout(function() {   // ask angular kindly to re-digest after this
-                gpsService.setPort($scope.selectedPort, $scope.baudRate);
-            }, 1);
+            gpsService.setPort($scope.selectedPort, $scope.baudRate);
         };
-
-        $scope.maxReads = 32;
-        $scope.gpsReads = 0;
-
-        gpsService.onRead(function(data) {
-
-            // TODO: we may want to give this a few tries ...
-            if(data.lat) {
-                $scope.serialConfigurationWorks = true;
-
-                // TODO: unsubscribe self instead of clear all
-                gpsService.clearEvents();
-            } else {
-                console.log(data);
-
-                if($scope.gpsReads == $scope.maxReads) {
-                    gpsService.clearEvents();
-                } else {
-                    $scope.gpsReads++;
-                }
-            }
-
-            // TODO: close port ...
-        })
-
-        // TODO: Close port
 
         $scope.setPort = function() {
 
@@ -91,6 +86,10 @@
 
         $scope.autoConfigure = function() {
 
+        }
+
+        $scope.openSerialPorts = function() {
+            return gpsService.openSerialPorts;
         }
 
         $scope.init();
