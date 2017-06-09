8e9ef320b6c3834b8a27f08794e49378332cea3d
EricWehrly <eric@disruptiontheory.com>
2017-06-08 22:05:24 -0400

    linux friendly menu

--- app/scripts/services/service.menu.js
+++ app/scripts/services/service.menu.js
@@ -1,50 +1,73 @@
 (function () {
     'use strict';
     angular.module('neteoc').service('menu', menu);
-    menu.$inject = ['$rootScope', '$location', 'Menu', 'currentWindow'];
+    menu.$inject = ['$rootScope', '$location', 'Menu', 'currentWindow', 'os'];
 
-    function menu($rootScope, $location, Menu, currentWindow) {
+    function menu($rootScope, $location, Menu, currentWindow, os) {
         this.init = function () {
 
-            var menu = Menu.buildFromTemplate([
-            {
+            // var os = require('os');
+
+            var mapMenu = {
                 label: 'Map',
                 click: function () {
                     $location.path("/gis");
                     $rootScope.$apply();
                 }
-            },
-            {
-                label: 'Devices',
-                submenu: [{
-                    label: 'GPS',
-                    submenu: [{
-                        label: "Serial Ports",
-                        click: function () {
-                            $location.path("/serialports");
-                            $rootScope.$apply();
-                        }
-                    }, {
-                        label: "Fake Device 2",
-                        click: function () {
-                            alert('Now Connected To Fake Device 2');
-                        }
-                    }]
-                }]
-            },
-            {
-                label: 'Dev Tools',
-                click: function () { currentWindow.webContents.openDevTools(); }
-            },
-            {
+            };
+
+            var settingsMenu = {
                 label: 'Settings',
                 click: function () {
                     $location.path("/settings");
                     $rootScope.$apply();
                 }
+            };
+
+            var mapMenuLinux = {
+                label: 'Settings',
+                submenu: [{
+                    label: 'Mandatory extra Linux button',
+                    click: function () {
+                        $location.path("/gis");
+                        $rootScope.$apply();
+                    }
+                }]
+            };
+
+            var settingsMenuLinux = {
+                label: 'Settings',
+                submenu: [{
+                    label: 'Linux is broken so here\'s an unneccessary submenu',
+                    click: function () {
+                        $location.path("/settings");
+                        $rootScope.$apply();
+                    }
+                }]
+            };
+
+            if(os.platform() == "linux") {
+                var menuTemplate = [
+                    mapMenuLinux,
+                {
+                    label: 'Dev Tools',
+                    click: function () { currentWindow.webContents.openDevTools(); }
+                },
+                settingsMenuLinux
+                ];
+
+            } else {
+                var menuTemplate = [
+                    mapMenu,
+                {
+                    label: 'Dev Tools',
+                    click: function () { currentWindow.webContents.openDevTools(); }
+                },
+                settingsMenu
+                ];
             }
-            ]);
-            Menu.setApplicationMenu(menu);
+
+            Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
         }
     };
 })();
